c15b791f3c5e40f435b874839f439269fe7e8a3c
Falk92 <f.heger@zeppelin-university.net>
2016-08-23 17:49:27 +0200

    bug fix background

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'static_pages/contact'
+
+  get 'static_pages/events'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
