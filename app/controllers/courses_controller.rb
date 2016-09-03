49be73bc3ad3af15b78bfa57f42dda598b99d3e8
bkroggel <b.kroggel@yahoo.de>
2016-09-03 14:19:23 +0200

    added button style on new and edit

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -78,7 +78,7 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:title, :time, :place, :description, :image_url)
+      params.require(:course).permit(:title, :time, :place, :description, :image_url, :fromtime, :totime, :material)
     end
 
 
